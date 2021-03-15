e324be6da2431f1a7ad7f9cfc3aa7a3790e612db
Henry Nichols <nicholsh562@gmail.com>
2021-03-15 13:24:32 -0700

    Fixed-JavaScript-Operators-and-Experssions

--- javascript-operators-and-expressions/main.js
+++ javascript-operators-and-expressions/main.js
@@ -4,14 +4,14 @@ var height = 70;
 var area = width * height;
 
 console.log('value of area:', area);
-console.log('typeof area:', typeof 3500);
+console.log('typeof area:', typeof area);
 
 var bill = 1500;
 var payment = 1600;
 var change = payment - bill;
 
 console.log('value of change:', change);
-console.log('typeof change:', typeof 100);
+console.log('typeof change:', typeof change);
 
 var quizzes = 100;
 var midterm = 50;
@@ -19,28 +19,28 @@ var final = 90;
 var grade = ((quizzes + midterm + final) / 3);
 
 console.log('value of grade:', grade);
-console.log('typeof grade:', typeof 80);
+console.log('typeof grade:', typeof grade);
 
 var firstName = 'Henry';
 var lastName = 'Nichols';
 var fullName = firstName + ' ' + lastName;
 
 console.log('value of fullName:', fullName);
-console.log('typeof fullName:', typeof 'Henry Nichols');
+console.log('typeof fullName:', typeof fullName);
 
 var pH = 10;
 var isAcidic = (pH > 7);
 
 console.log('value of isAcidic:', isAcidic);
-console.log('typeof isAcidic:', typeof true);
+console.log('typeof isAcidic:', typeof isAcidic);
 
 var headCount = 30;
 var isSparta = headCount === 300;
 
 console.log('value of isSparta:', isSparta);
-console.log('typeof isSparta', typeof false);
+console.log('typeof isSparta', typeof isSparta);
 
 var motto = fullName + ' ' + 'is the GOAT';
 
 console.log('value of motto:', motto);
-console.log('typeof motto:', typeof 'Henry Nichols is the GOAT');
+console.log('typeof motto:', typeof motto);
