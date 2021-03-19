aa3b2e8656e940dc076887424dbe941626008a4d
Henry Nichols <nicholsh562@gmail.com>
2021-03-19 10:53:09 -0700

    Fixed Errors

--- javascript-loops-practice/add-suffix-to-all.js
+++ javascript-loops-practice/add-suffix-to-all.js
@@ -1,9 +1,11 @@
 /* exported addSuffixToAll */
 function addSuffixToAll(words, suffix) {
+  var results = [];
   for (var i = 0; i < words.length; i++) {
     var temp = words[i];
     temp = temp + suffix;
     words[i] = temp;
+    results.push(words[i]);
   }
-  return words;
+  return results;
 }
