5bd7d4ee03be2211d1b28756ecb0827e9eb3725e
Henry Nichols <nicholsh562@gmail.com>
2021-03-19 16:28:44 -0700

    Fixed Errors

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,8 +1,9 @@
 /* exported isUpperCased */
 function isUpperCased(word) {
-  if (word === word.toUpperCase()) {
-    return true;
-  } else {
-    return false;
-  }
+  // if (word === word.toUpperCase()) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  return word === word.toUpperCase();
 }
