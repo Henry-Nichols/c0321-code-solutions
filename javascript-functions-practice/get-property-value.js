bed15e146e0b0cc16508537f2ca84341e9e70922
Henry Nichols <nicholsh562@gmail.com>
2021-03-18 18:22:30 -0700

    Fixed Errors

--- javascript-functions-practice/get-property-value.js
+++ javascript-functions-practice/get-property-value.js
@@ -1,18 +1,5 @@
 /* exported getPropertyValue */
-function getPropertyValue(obj, key) {
-
-  if (key === 'birthPlace') {
-    var props = Object.entries(obj);
-    return props[2][1];
-  }
-
-  if (key === 'occupation') {
-    var props2 = Object.entries(obj);
-    return props2[1][1];
-  }
-
-  if (key === 'name') {
-    var props3 = Object.entries(obj);
-    return props3[0][1];
-  }
+function getPropertyValue(object, key) {
+  var result = object[key];
+  return result;
 }
