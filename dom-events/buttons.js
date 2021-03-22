7bdfeabd8f1eb957fa8f5b6c9c2f038cc9436dee
Henry Nichols <nicholsh562@gmail.com>
2021-03-22 15:47:35 -0700

    Fixed-Dom-Events

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,20 +1,35 @@
 var buttonOne = document.querySelector('.click-button');
-buttonOne.addEventListener('click', function handleClick(event) {
+
+function handleClick(event) {
   console.log('button clicked');
   console.log('event:', event);
   console.log('event.target:', event.target);
+}
+
+buttonOne.addEventListener('click', function () {
+  handleClick(event);
 });
 
 var buttonTwo = document.querySelector('.hover-button');
-buttonTwo.addEventListener('mouseover', function handleMouseOver(event) {
+
+function handleMouseOver(event) {
   console.log('button hovered');
   console.log('event:', event);
   console.log('event.target:', event.target);
+}
+
+buttonTwo.addEventListener('mouseover', function () {
+  handleMouseOver(event);
 });
 
 var buttonThree = document.querySelector('.double-click-button');
-buttonThree.addEventListener('dblclick', function handleDoubleClick(event) {
+
+function handleDoubleClick(event) {
   console.log('button double-clicked');
   console.log('event:', event);
   console.log('event.target:', event.target);
+}
+
+buttonThree.addEventListener('dblclick', function () {
+  handleDoubleClick(event);
 });
