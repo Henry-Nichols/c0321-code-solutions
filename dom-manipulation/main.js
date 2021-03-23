239ff7a5d2d8a820851afa4fb5471cebe3dd6b93
Henry Nichols <nicholsh562@gmail.com>
2021-03-23 11:30:47 -0700

    Fixed-Dom-Manipulation

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -3,47 +3,31 @@ var clickCount = document.querySelector('.click-count');
 var btn = document.querySelector('#btn');
 var clickCounter = 0;
 
-hotButton.addEventListener('click', function () {
+hotButton.addEventListener('click', function (event) {
   clickCounter++;
   clickCount.textContent = 'Clicks:' + ' ' + clickCounter;
   if (clickCounter < 4) {
-
-    if (!btn.classList.contains('cold')) {
-      btn.classList.add('cold');
-    }
+    btn.classList.add('cold');
 
   } else if (clickCounter < 7) {
     btn.classList.remove('cold');
-
-    if (!btn.classList.contains('cool')) {
-      btn.classList.add('cool');
-    }
+    btn.classList.add('cool');
 
   } else if (clickCounter < 10) {
     btn.classList.remove('cool');
-
-    if (!btn.classList.contains('tepid')) {
-      btn.classList.add('tepid');
-    }
+    btn.classList.add('tepid');
 
   } else if (clickCounter < 13) {
     btn.classList.remove('tepid');
+    btn.classList.add('warm');
 
-    if (!btn.classList.contains('warm')) {
-      btn.classList.add('warm');
-    }
   } else if (clickCounter < 16) {
     btn.classList.remove('warm');
-
-    if (!btn.classList.contains('hot')) {
-      btn.classList.add('hot');
-    }
+    btn.classList.add('hot');
 
   } else if (clickCounter >= 16) {
     btn.classList.remove('hot');
-
-    if (!btn.classList.contains('nuclear')) {
-      btn.classList.add('nuclear');
-    }
+    btn.classList.add('nuclear');
   }
+
 });
