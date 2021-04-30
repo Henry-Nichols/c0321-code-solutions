07a92cd38856064cc57dd9c82adbd02a188800c3
Henry Nichols <nicholsh562@gmail.com>
2021-04-30 16:40:29 -0700

    Reivision-javascript-typing-tutor

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -2,7 +2,6 @@ var restart = document.querySelector('button');
 var typer = document.getElementById('phrase');
 var arr = typer.textContent.split('');
 var spanArr = [];
-<<<<<<< HEAD
 var phraseArr = [];
 var position = 0;
 
@@ -14,28 +13,18 @@ function loader() {
 }
 
 for (var i = 0; i < arr.length; i++) {
-=======
-var position =  0;
-
-for(var i = 0; i < arr.length; i++){
->>>>>>> b26ef0d8811d244e8244309aac27d9c92ff6f00d
   var spanCharacter = document.createElement('span');
   spanCharacter.textContent = arr[i];
   spanArr.push(spanCharacter);
 }
 
-<<<<<<< HEAD
 window.addEventListener('keyup', function (e) {
-=======
-window.addEventListener('keyup', function(e) {
->>>>>>> b26ef0d8811d244e8244309aac27d9c92ff6f00d
   if (arr[position].toLowerCase() === e.key) {
 
     spanArr[position].classList.remove('incorrect');
     spanArr[position].classList.add('correct');
 
     spanArr[position].classList.remove('underline');
-<<<<<<< HEAD
     spanArr[position + 1].classList.add('underline');
 
     typer.innerHTML = '';
@@ -48,26 +37,10 @@ window.addEventListener('keyup', function(e) {
 
   } else {
     spanArr[position].classList.add('incorrect');
-=======
-    spanArr[position+1].classList.add('underline');
-
-    typer.innerHTML = '';
-
-    for(var i = 0; i < arr.length; i++){
-      typer.appendChild(spanArr[i]);
-    }
-
-    position++
-
-  }else{
-    spanArr[position].classList.add('incorrect');
-
->>>>>>> b26ef0d8811d244e8244309aac27d9c92ff6f00d
   }
 
 });
 
-<<<<<<< HEAD
 restart.addEventListener('click', function (e) {
   var iterator = spanArr.values();
   for (var el of iterator) {
@@ -76,8 +49,3 @@ restart.addEventListener('click', function (e) {
     el.classList.remove('underline');
   }
 });
-=======
-restart.addEventListener('click', function(e) {
-  window.location.reload();
-})
->>>>>>> b26ef0d8811d244e8244309aac27d9c92ff6f00d
