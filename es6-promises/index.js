fb9b4a95a0f8cefbd5fc59b4a5802196ba67f2ee
Henry Nichols <nicholsh562@gmail.com>
2021-05-10 18:06:39 +0000

    Revised-es6-promises

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -4,6 +4,6 @@ const promise = takeAChance('Henry Nichols');
 
 promise.then(message => {
   console.log(message)
-}).catch(message => {
-  console.log(message)
+}).catch(error => {
+  console.log(error.message)
 });
